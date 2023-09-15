e78abd85766e18482824d586d15e16e578172ff9
Rebecca Hass <rebecca.hass91@googlemail.com>
2023-09-15 11:39:05 +0200

    fix local storage

--- script.js
+++ script.js
@@ -2,12 +2,52 @@ const prepDescription = document.getElementById("description");
 const btnAdd = document.getElementById("btn-add");
 const prepList = document.getElementById("prep-list");
 
-const prepFilter = document.querySelectorAll("#prep-filter-list input");
+// tasks default state
 
+const state = {
+  prepTasks: [
+    { description: "check passport validity", done: true, id: 1 },
+    { description: "pack camera", done: false, id: 2 },
+  ],
+};
+
+btnAdd.addEventListener("click", addPrepTask);
+
+// add tasks
+prepList.addEventListener("change", updateTask);
+
+function restoreTasks() {
+  const storedTasks = JSON.parse(localStorage.getItem("prepTasks"));
+  console.log("storedTasks", storedTasks);
+  if (storedTasks) {
+    state.prepTasks = storedTasks;
+  }
+}
+
+function addPrepTask(event) {
+  event.preventDefault();
+
+  const newPrepTask = {
+    description: prepDescription.value,
+    done: false,
+    id: Math.floor(Math.random() * 5000000),
+  };
+
+  // push tasks
+  state.prepTasks.push(newPrepTask);
+
+  //load tasks into local storage
+  localStorage.setItem("prepTasks", JSON.stringify(state.prepTasks));
+
+  restoreTasks();
+  renderPrepTasks();
+}
+
+// show tasks by selected filter
+
+const prepFilter = document.querySelectorAll("#prep-filter-list input");
 prepFilter.forEach((prepFilter) => {
-  prepFilter.addEventListener("change", () => {
-    renderPrepTasks();
-  });
+  prepFilter.addEventListener("change", () => {});
 });
 
 function getSelectedFilter() {
@@ -20,37 +60,6 @@ function getSelectedFilter() {
   });
 
   return selectedFilter;
-}
-
-const state = {
-  prepTasks: [
-    { description: "check passport validity", done: true, id: 1 },
-    { description: "pack camera", done: false, id: 2 },
-  ],
-};
-
-btnAdd.addEventListener("click", addPrepTask);
-
-prepList.addEventListener("change", updateTask);
-
-function addPrepTask(event) {
-  event.preventDefault();
-  const newPrepTask = {
-    description: prepDescription.value,
-    done: false,
-    id: Math.floor(Math.random() * 5000000),
-  };
-
-  state.prepTasks.push(newPrepTask);
-  localStorage.setItem("prepTasks", JSON.stringify(state.prepTasks));
-
-  const storedTask = JSON.parse(localStorage.getItem("prepTasks"));
-
-  if (storedTask) {
-    state.prepTasks = storedTask;
-  }
-
-  renderPrepTasks();
 }
 
 function renderPrepTasks() {
@@ -90,7 +99,7 @@ function renderPrepTasks() {
     prepList.appendChild(listEl);
   });
 }
-
+// read prepTaskID from DOM
 function updateTask(event) {
   const id = event.target.prepTaskId;
 
@@ -101,32 +110,16 @@ function updateTask(event) {
   updatedTask.done = event.target.checked;
   console.log(state.prepTasks);
 }
-
-function removeDoneTask(event) {
-  const taskId = event.target.id;
-  const prepTasks = state.prepTasks;
-  console.log(taskId);
-
-  const taskToUpdate = prepTasks.find(function (prepTask) {
-    return prepTask.id.toString === taskId;
-  });
-  if (doneTask) {
-    taskToUpdate.done = event.target.checked;
-  }
-}
-
-function removeDuplicateItem(event) {
-  const duplicateId = event.target.prepTaskId;
-  const duplicateTask = state.prepTasks;
-
-  const duplicateItemIndex = duplicateTask.findIndex(function (prepTask) {
-    return prepTask.description === event.target.description;
-  });
-
-  if (duplicateItemIndex !== -1) {
-    duplicateTask.splice(duplicateItemIndex, 1);
-  }
-}
+restoreTasks();
 renderPrepTasks();
 
-//
+//remove all done task (button remove is clicked)
+
+//const btnRemove = document.getElementById("btn-remove");
+
+//function removeDuplicateTasks {
+//  if (prepTasks) {
+//    return prepTasks = prepTasks.filter
+//  }
+//}
+//  btnRemove.addEventListener("click", removeDuplicateItem);
